dd31673f8b02acb9d7a5bc5700ba16c6c2d44f3d
Hygie Lichtenthaeler <hlichten@c1r5s2.42lausanne.ch>
2024-11-11 16:31:27 +0100

    mise en place

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlichten <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 15:33:43 by hlichten          #+#    #+#             */
+/*   Updated: 2024/11/11 15:37:35 by hlichten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
